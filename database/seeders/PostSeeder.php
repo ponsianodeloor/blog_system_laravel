37fda3539b2a00dfcb7e91d85bea90d7fe3d0a64
Ponsiano De Loor <ponsianodeloor@gmail.com>
2022-10-08 10:45:53 -0500

    modificar el seeder post para la relacion muchos a muchos con el modelo tag
    se registra en una tabla post_tag por cada post 2 etiquetas

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Image;
 use App\Models\Post;
+use App\Models\Tag;
 use Illuminate\Database\Seeder;
 
 class PostSeeder extends Seeder
@@ -15,6 +16,19 @@ class PostSeeder extends Seeder
             Image::factory(1)->create([
                 'imageable_id' => $post->id,
                 'imageable_type' => Post::class
+            ]);
+            /**
+             * se puede realizar un random del 1 al 8 pero se podrian repetir los tags
+             * por lo que al crear 8 tags indicamos el rango 1 al 4 para el primer post
+             * y del 5 al 8 para el segundo post
+            $post->tags()->attach([
+                Tag::all()->random()->id,
+                Tag::all()->random()->id
+            ]);
+             **/
+            $post->tags()->attach([
+                rand(1,4),
+                rand(5,8)
             ]);
         }
     }
